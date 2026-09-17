d5b2cf1b0e7ce49fb6055579da474d546cc84e1e
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-09-16 23:38:29 -0500

    a

--- minerva/src/main/java/com/minerva/domain/entities/sale/ProductSale.java
+++ minerva/src/main/java/com/minerva/domain/entities/sale/ProductSale.java
@@ -1,10 +1,14 @@
 package com.minerva.domain.entities.sale;
 
+import com.minerva.domain.constants.Modifier;
 import com.minerva.domain.entities.product.ProductId;
 import com.minerva.domain.valueObject.Money;
+
+import java.util.Optional;
 
 public interface ProductSale {
     ProductId getId();
     Money getCost();
     Money getPrice();
+    Optional<Money> getModifierPrice(Modifier modifier);
 }
